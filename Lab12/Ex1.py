3a25f2a1eda500f10188e51d4dd86b03765e903b
Zuhmeer <zingram@hawaii.edu>
2024-11-03 21:53:05 -1000

    Completed lab 12

---
+++ Lab12/Ex1.py
@@ -0,0 +1,20 @@
+# Open a url and print out some content from it. Print the instances of the <title> tag
+import urllib.request
+import ssl 
+
+ssl._create_default_https_context = ssl._create_unverified_context 
+
+url = "https://data.cityofchicago.org/Historic-Preservation/Landmark-Districts/zidz-sdfj/about_data"
+encoding = 'utf-8'
+
+# open the web page 
+print(f"Opening {url}...")
+webpage = urllib.request.urlopen(url)
+print(webpage)
+
+#iterate through each line in the webpage and search for <title>
+for line in webpage:
+    line = line.decode(encoding)
+    if '<title>' in line:
+        print(line)
+        
